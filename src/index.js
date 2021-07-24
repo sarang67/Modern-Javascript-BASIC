aaf81c12d03172e74f6b73c2c803dbc733985b0c
sarang <sarang>
2021-07-24 16:25:46 +0530

    2) let block scoping

--- src/index.js
+++ src/index.js
@@ -5,3 +5,28 @@ app.innerHTML = `
   <h1>Modern JavaScript: Basic</h1>
 `;
 
+//diffrence : scoping function scope and block scope , and cant access before declaring
+
+console.log(z);
+console.log(y);
+
+var z = "test";
+
+let y = "test";
+
+function one() {
+  if (true) {
+    var a = 123;
+  }
+  console.log(a);
+}
+
+{
+  let b = 123;
+}
+
+one();
+console.log(b);
+
+//for(){}
+//if(){}
