0da41f4d584b304a3fd5fa71e66776b0036d14ea
sarang <sarang>
2021-07-25 21:29:41 +0530

    show country data using xml http request

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 import "../assets/css/style.css";
 
-const app = document.getElementById("app");
-app.innerHTML = `
-  <h1>Modern JavaScript: Basic</h1>
-`;
+const countriesElement = document.querySelector(".countries");
+
+const getCountry = function (countryName) {
+  const request = new XMLHttpRequest();
+  request.open("get", `https://restcountries.eu/rest/v2/name/${countryName}`);
+  request.send();
+
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(request.responseText);
+    const { name, region, flag, population, languages, currencies } = data;
+
+    const counteriesHtml = `
+        <article class="country">
+            <img class="country__img" src="${flag}" />
+            <div class="country__data">
+                <h3 class="country__name">${name}</h3>
+                <h4 class="country__region">${region}</h4>
+                <p class="country__row"><span>👫${population}</span></p>
+                <p class="country__row"><span>🗣️ ${languages[0].name}</span></p>
+                <p class="country__row"><span>💰 ${currencies[0].name}</span></p>
+            </div>
+        </article>
+      `;
+
+    console.log(counteriesHtml);
+    countriesElement.innerHTML += counteriesHtml;
+    countriesElement.style.opacity = 1;
+  });
+};
+
+getCountry("portugal");
+getCountry("germany");
+getCountry("bangladesh");
