da71d99a8b0e9d3f11ad3ebce359823b7e82996d
sarang <sarang>
2021-07-25 17:01:59 +0530

    string in depth

--- src/index.js
+++ src/index.js
@@ -7,129 +7,51 @@ app.innerHTML = `
 
 /*
 
-1) ****** How to craete Number in javascript***********
+//boolean literal
 
-// literal type
-const literalNo = 10; // new Number(10)
-const literalNo2 = "10";
+let isMarried = true;
+let hasChild = false;
 
-console.log(literalNo);
-console.log(literalNo2);
+console.log(isMarried);
+console.log(hasChild);
 
-// function syntax :-
-console.log(Number(2));
-console.log(Number("222"));
-console.log(Number("222px"));
-console.log(Number({}));
-console.log(Number([1, 2, 3]));
-console.log(Number(""));
-console.log(Number(true));
-console.log(Number(false));
+// type conversion to boolean
 
-// object using function constructor and new keyword
+console.log(Boolean(10));
+console.log(Boolean(true));
+console.log(Boolean(false));
+console.log(Boolean(""));
+console.log(Boolean(0));
+console.log(Boolean([]));
+console.log(Boolean({}));
+console.log(Boolean(undefined));
 
-const myNOOBJ = new Number(50);
-console.log(myNOOBJ);
+// varbale useing function construtor using new keyword
 
-2) ****** How to handle decimal Number in javascript***********
-
-// decimal javascript
-console.log(0.1 + 0.2);
-console.log(100 / 3);
-
-console.log(33.333333333333336 * 3);
-
-const price = 9.33;
-const quantity = 3;
-
-console.log(9.33 * 3);
+console.log(new Boolean(true));
 
 
+// ************immutability in boolean ****************
+var ismarried = true;
+var second = ismarried;
+ismarried = false;
 
-const anotherPrice = 9.33 * 100;
-console.log(anotherPrice);
-const anotherQty = 3;
-
-console.log((anotherPrice * anotherQty) / 100);
-
-===============================
-
-3) ****** How to parse number in Javascript***********
-
-console.log(parseInt(10.25));
-console.log(parseInt("10px"));
-console.log(parseInt("10px10"));
-console.log(parseInt("px10"));
-
-console.log(parseInt(1011, 10));
-console.log(parseInt(1011, 8));
-console.log(parseInt(1011, 16));
-console.log(parseInt(1011, 2));
-
-console.log(parseFloat(5));
-console.log(parseFloat(5.25));
-console.log(parseFloat("5.25"));
-console.log(parseFloat("5.25px"));
-
-console.log(Number(10.25));
-console.log(Number("10.25"));
-console.log(Number("10px"));
-console.log(Number("10px10"));
-console.log(Number("px10"));
-
-console.log(+"9999");
-console.log(-"9999");
-
-console.log(10 + +"9999");
-==========================
-
-4) ****** How to handle NAN in js***********
-
-//Number.isNaN(NAN);
-
-console.log(Number.isNaN(10));
-console.log(Number.isNaN("sarang jain"));
-console.log(Number.isNaN(5 * "sarang"));
-
-console.log(Number.isInteger(10));
+console.log(ismarried);
+console.log(second);
 
 
-5) ****** ****** all premituve type , number are immutable , means originol value can not be changed that will be lost in memory*****
+// ************bolean typechecking ***************
 
-let immutableNo = 10;
-let otherNO = immutableNo;
+console.log(typeof true);
+console.log(typeof false);
+console.log(new Boolean(true) instanceof Boolean);
 
-immutableNo = 10 + 20;
-otherNO = otherNO + 1;
 
-console.log(immutableNo);
-console.log(otherNO);
 
-=====================================
-6) ****** How to type checking of number type***********
+// **********Boolean prototype method ************
+console.dir(Boolean.prototype);
 
-//type check with number
-
-console.log(typeof 100);
-console.log(new Number(10) instanceof Number);
-console.log(Number(10) instanceof Number);
-console.log(99 instanceof Number);
-
-7) ****** number prototype property***********
-
-//Number prototype property (methods)
-
-console.log(Number.prototype);
-
-console.log((99).toFixed());
-console.log((99.66).toFixed());
-console.log((99.1234).toFixed());
-
-console.log((99.456).toPrecision(3));
-console.log((99.456).toString());
-console.log((5).toString());
-
-console.log(new Number(100).valueOf());
-
+console.log(true.toString());
+console.log(new Boolean(true).valueOf());
 
 */
