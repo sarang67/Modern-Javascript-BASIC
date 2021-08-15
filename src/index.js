67945f31e95c918acc2fc4061afadb14b028854f
sarang <sarang>
2021-08-15 17:07:05 +0530

    Object in depth first

--- src/index.js
+++ src/index.js
@@ -4,256 +4,262 @@ const app = document.getElementById("app");
 app.innerHTML = `
   <h1>Modern JavaScript: Basic</h1>
 `;
+// Object in depth
 
-// Function in depth 3
+// Object in depth
+// object literal
+const myobj = {};
+console.log(myobj);
 
-// Function in depth
-// Nature of this keyword and call apply bind
-
-/*
-        
-        1) any function ---> this ---> global object
-        2) any method  ----> this ----> self object
-        3) short function  (this behaviour free) ---> this ---> it will take this behaviour from where this is written 
-        (check in above line where this is written)
-
-
-        in case of strict mode 
-        1) any function ---> this ---> undefined
-         
-  */
-
-// Function in depth
-// Nature of this keyword and call apply bind
-
-console.log(this); // window , global object
-
-var checkagainthis5 = () => {
-  console.log(this); // window , global object
+const personalInformation = {
+  name: "sarang jain",
+  age: 32,
+  phoneNo: 9405034908,
+  isMaried: true,
+  profession: "Teacher",
+  friends: ["kk", "amar", "adarsh"],
+  address: {
+    flatNo: 201,
+    street: "Gwalior",
+    state: "MP",
+    pincode: "411014",
+    landmark: ["jain temple", "gwalior uravai gate"],
+  },
+  getPerdonalInformation: function () {
+    console.log(this.name, this.age, this.phoneNo);
+  },
 };
 
-checkagainthis5();
+console.log(personalInformation);
 
-function checkthis() {
-  console.log(this); // window , global object
-}
+// using function constructor
+var info1 = new Object();
+console.log(info1);
 
-checkthis();
+var info2 = Object();
+console.log(info2);
 
-let checkthis2 = function () {
-  console.log(this); // window , global object
-};
+console.log(Object(3));
+console.log(Object("sarang"));
+console.log(Object(true));
+console.log(Object([]));
+console.log(Object(() => {}));
 
-checkthis2();
+// immutability with object
 
-let obj = {
-  myName: "sarang",
+let myInfo = { name: "sarang", age: 30 };
+
+let amitInfo = myInfo;
+
+console.log(myInfo);
+console.log(amitInfo);
+
+amitInfo.lastname = "bablekar";
+
+console.log(myInfo);
+console.log(amitInfo);
+
+let kkInfo = amitInfo;
+kkInfo.name = "kk";
+
+console.log(myInfo);
+console.log(amitInfo);
+console.log(kkInfo);
+
+// *************Object properties and method value
+let personlInfo = {
+  name: "sarang",
   age: 30,
   getInfo: function () {
-    console.log(this); // self object
-    console.log(this.myName, this.age);
-
-    // solution  till 2015
-    var self = this;
-
-    function checkagainthis() {
-      console.log(this); // window , global object
-      console.log(self.myName, self.age);
-    }
-
-    checkagainthis();
-
-    // short function
-
-    var checkagainthis1 = () => {
-      console.log(this); // self object
-      console.log(this.myName, this.age); // self object
-    };
-
-    checkagainthis1();
-  },
-};
-
-console.log(obj.getInfo());
-
-/*
-// call apply bind
-// when we use function as a object we use these methdo, we can change the context for which function will call for which context
-
-  call(context) --> immediate call the function , argument is given simple form
-  apply(context) --> immediate call the function , argument is given array form
-  bind(context) --> will not immediate call the function, this will give new defination of function for later call
-
-      first argument would be ---> will be context object
-
-
-      you are changing the behaviour of this, using call apply bind.
-  */
-
-function sayHello() {
-  console.log("Hello students");
-}
-
-sayHello();
-sayHello.call();
-sayHello.apply();
-let newBindDefination = sayHello.bind();
-console.log(newBindDefination);
-newBindDefination();
-
-function add(a, b) {
-  console.log(a + b);
-}
-
-add(10, 10);
-add.call(this, 10, 20);
-add.apply(this, [10, 20]);
-
-let newAdd = add.bind(this, 10, 20);
-console.log(newAdd);
-newAdd();
-
-// what is context , what is parameter
-
-function checkThis() {
-  console.log(this);
-}
-
-checkThis();
-checkThis.call(undefined);
-checkThis.call("amit");
-checkThis.call([]);
-checkThis.call({});
-checkThis.call(true);
-checkThis.call(null);
-checkThis.apply(new Date());
-checkThis.apply({ name: "sarang", age: 35 });
-checkThis.apply([1, 2, 3, 4]);
-
-let myInformation = {
-  name: "sarang jain",
-  age: 35,
-  getInfo: function () {
-    console.log(this.name, this.age);
-  },
-};
-
-let amitInformation = {
-  name: "amit",
-  age: 32,
-  getInfo: function () {
-    console.log(this.name, this.age);
-  },
-};
-
-let darshanformation = {
-  name: "darshan",
-  age: 25,
-  getInfo: function () {
-    console.log(this.name, this.age);
-  },
-};
-
-myInformation.getInfo();
-amitInformation.getInfo();
-darshanformation.getInfo();
-
-let myInformation1 = {
-  name: "sarang jain",
-  age: 35,
-};
-
-let amitInformation1 = {
-  name: "amit",
-  age: 32,
-};
-
-let darshanformation1 = {
-  name: "darshan",
-  age: 25,
-};
-
-function getInfo() {
-  console.log(this.name + this.age);
-}
-
-getInfo.apply(myInformation1);
-getInfo.apply(amitInformation1);
-getInfo.apply(darshanformation1);
-
-let myInformation2 = {
-  name: "sarang jain",
-  age: 35,
-  getInfo() {
     console.log(this.name + this.age);
   },
+  getInfo1() {
+    console.log(this.name + this.age);
+  },
+  "abc 123": "I am a valid property",
+  100: "i am also a valid property",
 };
 
-let amitInformation2 = {
-  name: "amit",
-  age: 32,
+console.log(personlInfo);
+
+console.log(personlInfo.name);
+console.log(personlInfo.age);
+
+console.log(personlInfo["abc 123"]);
+console.log(personlInfo["100"]);
+
+// ************Object properties and method value
+let personlInfo = {
+  name: "sarang",
+  age: 30,
+  getInfo: function () {
+    console.log(this.name + this.age);
+  },
+  getInfo1() {
+    console.log(this.name + this.age);
+  },
+  100: "no name property is also valid",
+  "my propery one": "this string is a valid property",
+  "my propery array": [1, 2, 3],
 };
 
-let darshanformation2 = {
-  name: "darshan",
-  age: 25,
-};
+console.log(personlInfo);
 
-myInformation2.getInfo();
+console.log(personlInfo.name);
+console.log(personlInfo.age);
 
-myInformation2.getInfo.call(amitInformation2);
+console.log(personlInfo[100]);
+console.log(personlInfo["100"]);
+console.log(personlInfo["my propery array"][2]);
 
-myInformation2.getInfo.apply(darshanformation2);
-
-// genric function for call apply bind
-function iterateArray() {
-  for (let i = 0; i < this.length; i++) {
-    console.log(this[i]);
-  }
-}
-
-let noarr = [1, 2, 3, 4, 5];
-let namearr = ["sarang", "kk", "govind"];
-let objarr = [1, 2, 3];
-
-iterateArray.call(noarr);
-iterateArray.apply(namearr);
-iterateArray.bind(objarr)();
-
-/// callback function
+// Object keys and shorthand
 
 /*
- when you are calling a function that is call a function call
- when one function is calling another function , this terminology called a callback function call
+  whene ever key and value are coming from same variable you can make short object
+  dynamic key , you can cretae key uisng variable like value , but using [] notation
+*/
+
+// let perosnalInfo = {
+//   name: "sarang",
+//   id: 123,
+//   age: 30,
+//   getInfo() {
+//     console.log(this.name, this.id, this.age);
+//   },
+// };
+
+// api , progrmattical way you are getting some value
+
+// let myName = "sarang";
+// let myId = "xyz";
+// let myage = 32;
+
+// let perosnalInfo2 = {
+//   name: myName,
+//   id: myId,
+//   age: myage,
+// };
+
+// console.log(perosnalInfo2);
+
+// // shorthand object , key and value are coming from same variable
+
+// let perosnalInfo3 = {
+//   myName, // myName:myName
+//   myId, // myId:myId
+//   myage,
+// };
+
+// console.log(perosnalInfo3);
+
+let myName = "sarang";
+let myId = "xyz";
+let myage = 32;
+
+let mykey = "salary";
+let key = "keyfrom api";
+
+let personInfo5 = {
+  myName,
+  myId,
+  myage: myage,
+  [mykey]: 30000,
+  [key]: "value",
+};
+
+console.log(personInfo5);
+
+// Object in depth
+// object destructuring
+
+// const trainName = courseInformation.trainer;
+// const trainId = courseInformation.id;
+// const fullFees = courseInformation.courseFess.full;
+//console.log(trainName, trainId, fullFees);
+
+// destructuring
+/*
+let { trainer, id, courseFess } = {
+  trainer: "sarang jain",
+  id: "webTrainer",
+  phoneNo: 9405034908,
+  city: "pune",
+  currentCourseName: "Javascript",
+  courseFess: { JS: 5000, react: 5000, full: 10000 },
+};
 
 */
 
-function sayHello() {
-  console.log("Hello");
-}
-
-sayHello();
-
-function sayHiLater(fn) {
-  console.log(
-    "Here sayHiLater function will call another function, that is called cb"
-  );
-
-  fn();
-}
-
-sayHiLater(function () {
-  console.log("say hello");
-});
-
-var sayHelloAgain = function () {
-  console.log("heyyyyyyyyyyyyy");
+let courseInformation = {
+  trainer: "sarang jain",
+  id: "webTrainer",
+  phoneNo: 9405034908,
+  city: "pune",
+  currentCourseName: "Javascript",
+  courseFess: { JS: 5000, react: 5000, full: 10000 },
 };
 
-sayHiLater(sayHelloAgain);
+//let { trainer, id, courseFess:{full} } = courseInformation;
 
-setTimeout(callBackFunction, 5000);
+// let { trainer, id, courseFess } = courseInformation;
+// let { full } = courseFess;
 
-function callBackFunction() {
-  console.log("i will run after 5 sec");
+// console.log(trainer);
+// console.log(id);
+// console.log(courseFess);
+// console.log(full);
+
+// const id = "xyz";
+
+// const { trainer, id: rename, phoneNo, courseFess, ...rest } = courseInformation;
+
+// console.log(trainer);
+// console.log(id);
+// console.log(rest);
+// console.log(rename);
+
+const { full } = courseInformation.courseFess;
+console.log(full);
+
+const { courseFess } = courseInformation;
+
+const { full: renamefull } = courseFess;
+console.log(renamefull);
+
+// const {
+//   courseFess: { full, react },
+// } = courseInformation;
+// console.log(full, react);
+
+// **************Object property value existence
+
+let courseInformation = {
+  trainer: "sarang jain",
+  id: "web trainer",
+  phoneNo: 9405034908,
+};
+
+console.log(courseInformation);
+
+// property check
+if (courseInformation.id) {
+  // if id exist then so something
 }
+
+// property value check
+if (courseInformation.id === "web trainer") {
+  // if id exist then so something
+}
+
+// Object.keys() , Object.values()
+
+// value check
+console.log(Object.values(courseInformation));
+const objectValues = Object.values(courseInformation);
+console.log(objectValues.includes("web trainer"));
+
+// if(Object.values(courseInformation).includes(9405034908)){}
+
+// keys check
+
+console.log(Object.keys(courseInformation).includes("trainer"));
