307dd67e236efad2778154768322a63696a281fd
sarang <sarang>
2021-08-22 17:03:54 +0530

    array in depth part first

--- src/index.js
+++ src/index.js
@@ -4,55 +4,147 @@ const app = document.getElementById("app");
 app.innerHTML = `
   <h1>Modern JavaScript: Basic</h1>
 `;
-// iteration of object loop : traditinal way
-
-let courseInformation = {
-  trainer: "sarang jain",
-  id: "webtrainer",
-  phoneNo: 9405034908,
-  courseandFees: { js: 5000, react: 5000, full: 5000 },
-};
-
-// for in loop (traditinal way)
-
-/*
-  for(cons prop in objectname){}
-*/
-
-for (const prop in courseInformation) {
-  console.log("first level key property--->  ", prop);
-  console.log(" first level value--->  ", courseInformation[prop]);
-
-  console.log("========================================");
-  // nested loop
-  if (
-    Object.prototype.toString.call(courseInformation[prop]) ===
-    "[object Object]"
-  )
-    for (const key in courseInformation[prop]) {
-      console.log("second level key property--->  ", key);
-      console.log(" secon level value--->  ", courseInformation[prop][key]);
-    }
-}
-
-console.log(courseInformation);
+// Array in depth
 
 
-// object in depth
+// literal way preferred
+const myInformation = [];
+console.log(myInformation);
 
-// iteration of object loop : modern way using Object.keys
+// Obeject , array are pass by refrence and they are mutable
+const myInformationRef = myInformation;
+myInformation.push(1);
+myInformationRef.push(2);
 
-let courseInformation = {
-  trainer: "sarang jain",
-  id: "webtrainer",
-  phoneNo: 9405034908,
-  courseandFees: { js: 5000, react: 5000, full: 5000 },
-};
+console.log(myInformationRef === myInformation);
+console.log([] === []);
 
-const keys = Object.keys(courseInformation);
-console.log(keys);
+console.log(myInformationRef);
+console.log(myInformation);
 
-for (let i = 0; i < keys.length; i++) {
-  const key = keys[i];
-  console.log(courseInformation[key]);
-}
+// Array function constructor
+
+console.log(new Array(1, 2, 3)); // below line also same
+console.log(Array(1, 2, 3)); // above line also same
+
+console.log(Array(1, 2, 3));
+console.log(Array(undefined));
+console.log(Array(null));
+console.log(Array("sarang"));
+console.log(Array({}));
+
+// JS Array is collection of anything
+
+const collection = [
+  1,
+  true,
+  null,
+  undefined,
+  { name: "sarang", age: 30 },
+  [1, 2, 3],
+  new Date(),
+  function () {},
+  new Object(),
+];
+console.log(collection);
+
+
+============
+// Array in depth
+
+// array indexes and properties
+const trainersInformation = ["sarang jain", "pune", "front end devloper"];
+console.log(trainersInformation);
+
+console.log(trainersInformation[0]);
+console.log(trainersInformation["1"]);
+console.log(trainersInformation[2]);
+
+// first element
+console.log("first element-->", trainersInformation[0]);
+
+// last element
+console.log(
+  "first element-->",
+  trainersInformation[trainersInformation.length - 1]
+);
+
+// emty your array
+// trainersInformation = [];
+//trainersInformation.length = 0;
+
+trainersInformation.splice(0, trainersInformation.length);
+
+console.log(trainersInformation);
+
+
+===============================
+
+// multidimenssion Array  : array inside array
+
+const trainercourseandFees = [
+  ["javascript", 5000],
+  ["React", 5000],
+  ["angular", 5000],
+];
+
+console.log(trainercourseandFees[0]);
+console.log(trainercourseandFees[0][0]);
+console.log(trainercourseandFees[0][1]);
+console.log(trainercourseandFees[1]);
+console.log(trainercourseandFees[1][0]);
+console.log(trainercourseandFees[1][1]);
+console.log(trainercourseandFees[2]);
+console.log(trainercourseandFees[2][0]);
+console.log(trainercourseandFees[2][1]);
+console.log(trainercourseandFees);
+
+
+
+// array is object you can craete propert apart from index
+var array = [1, 2, 3, 4];
+
+array["five"] = 5;
+array.six = 6;
+
+console.log(array);
+
+
+====================
+
+// Array in depth
+// array destructoiong
+
+const trainerFees = [
+  ["Javascript", 5000],
+  ["React", 5000],
+  ["Angular", 5000],
+  ["Vue", 5000],
+  ["Node", 5000],
+  ["ionic", 5000],
+  ["react-native", 5000],
+  ["html/css", 5000],
+  ["RWD/bootstrap/foundation", 5000],
+];
+
+console.log(trainerFees);
+const jsInformation = trainerFees[0];
+console.log(jsInformation[0], jsInformation[1]);
+
+const ionicInformation = trainerFees[5];
+console.log(ionicInformation[0], ionicInformation[1]);
+
+const [, reactInfo, angInfo, [vcourseName, vFees], ...rest] = trainerFees;
+
+console.log(reactInfo, angInfo, vcourseName, vFees, rest);
+
+console.log("----------------------------------");
+const arr = [1, 2, 3, 4];
+
+arr["hello"] = "hi";
+
+console.log(arr);
+
+const [one, two, three, four] = arr;
+const { hello } = arr;
+console.log(one, two, three, four, hello);
