76d04390ec0be724e18ee89181ad1aa44332afca
sarang <sarang>
2021-08-28 17:11:23 +0530

    array in depth part 2

--- src/index.js
+++ src/index.js
@@ -7,144 +7,161 @@ app.innerHTML = `
 // Array in depth
 
 
-// literal way preferred
-const myInformation = [];
-console.log(myInformation);
+/************
+ *  splice -->
+dlt the element  from array , and if you want to put element instad dlt elemet you can put new element using splice
+// param1 :_index from where you have to start dlt
+// param2 :- count how many no element you want dlt
+// param3 :- insert this item place of deleted item or place of( param1)
 
-// Obeject , array are pass by refrence and they are mutable
-const myInformationRef = myInformation;
-myInformation.push(1);
-myInformationRef.push(2);
+return --> array with dleted item
 
-console.log(myInformationRef === myInformation);
-console.log([] === []);
+*/
+//console.log(subject3.splice(0));
+//console.log(subject3.splice(0, 1));
+//console.log(subject3.splice(0, 2));
 
-console.log(myInformationRef);
-console.log(myInformation);
+//console.log(subject3.splice(1, 1, ["s", "s", "s"]));
 
-// Array function constructor
-
-console.log(new Array(1, 2, 3)); // below line also same
-console.log(Array(1, 2, 3)); // above line also same
-
-console.log(Array(1, 2, 3));
-console.log(Array(undefined));
-console.log(Array(null));
-console.log(Array("sarang"));
-console.log(Array({}));
-
-// JS Array is collection of anything
-
-const collection = [
-  1,
-  true,
-  null,
-  undefined,
-  { name: "sarang", age: 30 },
-  [1, 2, 3],
-  new Date(),
-  function () {},
-  new Object(),
-];
-console.log(collection);
+// add in between using splice
 
 
-============
-// Array in depth
-
-// array indexes and properties
-const trainersInformation = ["sarang jain", "pune", "front end devloper"];
-console.log(trainersInformation);
-
-console.log(trainersInformation[0]);
-console.log(trainersInformation["1"]);
-console.log(trainersInformation[2]);
-
-// first element
-console.log("first element-->", trainersInformation[0]);
-
-// last element
-console.log(
-  "first element-->",
-  trainersInformation[trainersInformation.length - 1]
-);
-
-// emty your array
-// trainersInformation = [];
-//trainersInformation.length = 0;
-
-trainersInformation.splice(0, trainersInformation.length);
-
-console.log(trainersInformation);
+/************
+ *  slice -->
+ * param --> starting index
+ * param2 --> ending index
+ *
+ * slice is immutable
 
 
-===============================
-
-// multidimenssion Array  : array inside array
-
-const trainercourseandFees = [
-  ["javascript", 5000],
-  ["React", 5000],
-  ["angular", 5000],
-];
-
-console.log(trainercourseandFees[0]);
-console.log(trainercourseandFees[0][0]);
-console.log(trainercourseandFees[0][1]);
-console.log(trainercourseandFees[1]);
-console.log(trainercourseandFees[1][0]);
-console.log(trainercourseandFees[1][1]);
-console.log(trainercourseandFees[2]);
-console.log(trainercourseandFees[2][0]);
-console.log(trainercourseandFees[2][1]);
-console.log(trainercourseandFees);
+Must Read deiffrence between splice and slice method 
+https://www.tothenew.com/blog/javascript-splice-vs-slice/
+https://www.freecodecamp.org/news/lets-clear-up-the-confusion-around-the-slice-splice-split-methods-in-javascript-8ba3266c29ae/
+https://dev.to/capscode/difference-between-slice-and-splice-in-javascript-4ahg
+ *
+ * */
 
 
 
-// array is object you can craete propert apart from index
-var array = [1, 2, 3, 4];
-
-array["five"] = 5;
-array.six = 6;
-
-console.log(array);
 
 
-====================
+//arrays-add-elements****************************
+/*
 
-// Array in depth
-// array destructoiong
+The splice() method changes the contents of an array by removing or replacing existing elements and/or adding new elements in place. To access part of an array without modifying it, see slice().
 
-const trainerFees = [
-  ["Javascript", 5000],
-  ["React", 5000],
-  ["Angular", 5000],
-  ["Vue", 5000],
-  ["Node", 5000],
-  ["ionic", 5000],
-  ["react-native", 5000],
-  ["html/css", 5000],
-  ["RWD/bootstrap/foundation", 5000],
-];
+*/
+/* 
+mutable :- means you are modifying the current or original array.
 
-console.log(trainerFees);
-const jsInformation = trainerFees[0];
-console.log(jsInformation[0], jsInformation[1]);
+immutable :- original value can not be disturbed , brand new array wiil be returned.
 
-const ionicInformation = trainerFees[5];
-console.log(ionicInformation[0], ionicInformation[1]);
+*/
 
-const [, reactInfo, angInfo, [vcourseName, vFees], ...rest] = trainerFees;
+const subjects = ["JS", "React", "Angular"];
 
-console.log(reactInfo, angInfo, vcourseName, vFees, rest);
+//add in begning Mutable way
+subjects.unshift("Vue");
 
-console.log("----------------------------------");
-const arr = [1, 2, 3, 4];
+//add in last Mutable way
+subjects.push("redux");
 
-arr["hello"] = "hi";
+console.log(subjects);
 
-console.log(arr);
+console.log("------------------------------");
 
-const [one, two, three, four] = arr;
-const { hello } = arr;
-console.log(one, two, three, four, hello);
+// immutable way
+const subjects1 = ["JS", "React", "Angular"];
+
+//add in begning Mutable way
+console.log(["Vue", ...subjects1]);
+
+//add in last Mutable way
+console.log(["Vue", ...subjects1, "redux"]);
+
+console.log(subjects1);
+console.log("------------------------------");
+
+
+// fisrt demonstrate splice method all the combination.
+
+// param 1 , delt index, second param delte count , thirt what you want to keep on param1 .
+splice(start)
+splice(start, deleteCount)
+splice(start, deleteCount, item1)
+splice(start, deleteCount, item1, item2, itemN)
+
+const subjects2 = ["JS", "React", "Angular"];
+// How to add in between mutable way
+
+subjects2.splice(1, 0, "Vue");
+console.log(subjects2);
+
+console.log("-----------------------------------");
+// How to add in between imutable way
+const subjects3 = ["JS", "React", "Angular", "React"];
+
+console.log([...subjects3.splice(0, 2), "Vue", ...subjects3.splice(0)]);
+
+console.log(subjects3);
+
+
+
+//array-remove*******************
+
+/*
+The slice() method returns a shallow copy of a portion of an array into a new array object 
+selected from begin to end (end not included) where begin and end represent the index of items in that array.
+The original array will not be modified.
+
+console.log(subjects.slice(0));
+console.log(subjects.slice(1));
+console.log(subjects.slice(2));
+console.log("--------------------------");
+console.log(subjects.slice(0, 0));
+console.log(subjects.slice(0, 1));
+console.log(subjects.slice(0, 2));
+
+console.log("--------------------------");
+console.log(subjects.slice(-1));
+console.log(subjects.slice(-2));
+console.log(subjects.slice(-3));
+console.log("--------------------------");
+
+console.log(subjects.slice(0, -1));
+console.log(subjects.slice(0, -2));
+
+
+*/
+
+// Array Remove
+
+const subjects = ["JS", "React", "Angular"];
+
+// removing from begnining -- mutatable way
+
+let removedItem = subjects.shift();
+console.log(removedItem);
+console.log(subjects);
+
+// removing from ending -- mutable way
+
+const subjects2 = ["JS", "React", "Angular"];
+const removedlast = subjects2.pop();
+
+console.log(subjects2);
+console.log(removedlast);
+
+// remove from anywhere immutable way using slice method
+// so make your mind and you can remove anything using slice method
+const subjects3 = ["JS", "React", "Angular", "redux"];
+
+// remove first element
+console.log([...subjects3.slice(1)]);
+// remove last element
+console.log([...subjects3.slice(0, subjects3.length - 1)]);
+
+// remove from enywhere
+const index = 2;
+
+console.log([...subjects3.slice(0, index), ...subjects3.slice(index + 1)]);
