bf8e9a969805938ccc08d20b10212922b7e65d6e
sarang <sarang>
2021-10-31 17:21:14 +0530

    **********************Forms and Events******************************** 1) access-forms-and-elements 2) submit-event-formdata extratc data 3) transform the formdata (using query string and json) 4) fetch-api-formdata (send form data)
    =============

--- src/index.js
+++ src/index.js
@@ -5,173 +5,254 @@ app.innerHTML = `
    <h1>Modern JavaScript: DOM</h1>
   `;
 
-// event-bubbling-capturing-propagation
-// =========================================
-// import "../assets/css/style.css";
-
-// const app = document.getElementById("app");
-// app.innerHTML = `
-//   <h1>JavaScript DOM</h1>
-//   <div class="one" style="border:1px solid red; padding:20px">
-//      <div class="two" style="border:1px solid green; padding:20px;margin:20px">
-//         <button class="three">click me</button>
-//       </div>
-//   </div>
-// `;
-
-// // We have three phase for our eevent propagate, first phase is capturing phase (event goes down to the element),
-// //second phase is target phase(wether the event reaches the target element), third phase is bubbling phase (whether the event up for the elemet)
-
-// //buubling phase is better than capturing phase , so if we make the third option of addEventListner is false (which is by default)
-// //means we want to use bubbling phase , or suppose we  make true third parameter , we want to use capturing phase
-
-// const one = document.querySelector(".one");
-// const two = document.querySelector(".two");
-// const three = document.querySelector(".three");
-
-// function handleClick(event) {
-//   // will stop propagation on next level
-//   // event.stopPropagation();
-//   // will stop immidiate propagation as well , means only one event handler will call, other attached will be avoided
-//   // event.stopImmediatePropagation();
-//   console.log(event.target);
-// }
-
-// one.addEventListener("click", handleClick);
-// two.addEventListener("click", handleClick);
-// three.addEventListener("click", handleClick);
-
-// three.addEventListener("click", (event) => console.log("===>", event));
-
-// preventing-default-event-actions
-// ====================================
-// import "../assets/css/style.css";
-
-// const app = document.getElementById("app");
-// app.innerHTML = `
-//   <h1>JavaScript DOM</h1>
-//    <form>
-//     <label>
-//       Sign-up Email
-//       <input type="email">
-//     </lable>
-//     <label>
-//       I agree to the terms
-//       <input type="checkbox">
-//     </label>
-//     <button type="submit">submit</button>
-//    </form>
-
-//    <a href="google.com">google</a>;
-//   `;
-
-// const form = document.querySelector("form");
-// const email = document.querySelector('input[type="email"]');
-// const checkbox = document.querySelector('input[type="checkbox"]');
-
-// const ancher = document.querySelector("a");
-
-// function handleSubmit(event) {
-//   if (!(email.value && checkbox.checked)) {
-//     event.preventDefault();
-//     console.log("form is not submitted");
-//     console.log(event.defaultPrevented);
-//     return;
-//   }
-//   console.log(event.defaultPrevented);
-//   console.log("i agree t&C , now form submitted");
-// }
-
-// form.addEventListener("submit", handleSubmit);
-
-// ancher.addEventListener("click", (event) => {
-//   event.preventDefault();
-// });
-
-// checkbox.addEventListener("click", (event) => {
-//   event.preventDefault();
-// });
-
-// event-delegation-dynamic-events:-
-// ===================================
-// import "../assets/css/style.css";
-
-// const app = document.getElementById("app");
-// app.innerHTML = `
-//   <h1>JavaScript DOM</h1>
-//   <button>Add Item</button>
-//   <ul id="list">
-//    <li>Item 1</li>
-//    <li>Item 2</li>
-//    <li>Item 3</li>
-//    <li>Item 4</li>
-//   </ul>
-// `;
-
-// const button = document.querySelector("button");
-// const list = document.querySelector("#list");
-// //const items = [...document.querySelectorAll("li")];
-
-// function handleCLick(event) {
-//   if (event.target.nodeName.toLowerCase() === "li") {
-//     console.log(event.target.innerText);
-//   }
-// }
-
-// // using of event delegation taking the advantage of bubbling effect,
-// //  and we are hanling event on parent once , and taking advanctage on child
-
-// list.addEventListener("click", handleCLick);
-
-// // items.forEach((item) => {
-// //   item.addEventListener("click", handleCLick);
-// // });
-
-// button.addEventListener("click", () => {
-//   const items = document.querySelectorAll("li");
-//   const li = document.createElement("li");
-
-//   li.innerText = `Item ${items.length + 1}`;
-
-//   // li.addEventListener("click", handleCLick);
-//   list.append(li);
-// });
+//**********************Forms and Events********************************
 
 /*
-
-06-keyboard-events:-
-=====================
+  1-access-forms-and-elements:-
+================================
 import "../assets/css/style.css";
 
 const app = document.getElementById("app");
 app.innerHTML = `
   <h1>JavaScript DOM</h1>
-  <div style="height:1000px;border:1px solid red"></div>
+  <form name="order">
+   <label>
+    Full Name
+    <input type="text" name="fullName"/>
+   </label>
+`;
+
+//const form = document.querySelector("form");
+//console.log(form);
+
+//const orderForm = document.forms[0];
+//console.log(orderForm);
+
+console.log(document.forms);
+
+const orderForm = document.forms.order;
+console.log(orderForm);
+
+console.log(orderForm.elements);
+
+const fullName = orderForm.elements.fullName;
+
+console.log(fullName);
+
+//const { fullName, email } = orderForm.elements;
+//console.log(fullName, email);
+
+function handleInput(event) {
+  // access the value
+  console.log(event.target.value);
+  //access the form
+  console.log(event.target.form);
+}
+// bind the input event
+fullName.addEventListener("input", handleInput);
+fullName.addEventListener("change", handleInput);
+   */
+
+/*****
+02-submit-event-formdata:-
+=========================
+
+import "../assets/css/style.css";
+
+const app = document.getElementById("app");
+app.innerHTML = `
+  <h1>JavaScript DOM</h1>
+    <form  name="order">
+      <label>
+        Your Name
+        <input type="text" name="fullname" />
+      </label>
+      <label>
+        Which pizza would you like?
+        <select name="pizza">
+          <option value="peproni">Peproni</option>
+          <option value="cheesy">Cheesy</option>
+          <option value="meety">Meety</option>
+        </select>
+      </label>
+      <input type="submit">
+      </form>
+
 
 `;
 
-document.addEventListener("keydown", (event) => {
-  //console.log(event);
-});
+const form = document.forms.order;
 
-document.addEventListener("keyup", (event) => {
-  //console.log(event.key, event.code);
+function handleSubmit(event) {
+  event.preventDefault();
+  new FormData(event.target);
+  //console.log([...new FormData(event.target)]);
+}
 
-  switch (event.key) {
-    case "a": {
-      console.log("I am key A");
-      break;
-    }
-    case "b": {
-      console.log("I am key B");
-      break;
-    }
-    case "ArrowUp": {
-      console.log("I am ArrowUp");
-      break;
-    }
+function handleformdata(event) {
+  //  console.log(event.formData);
+  //  console.log([...event.formData]);
+  //  console.log([...event.formData.values()]);
+  //  console.log([...event.formData.entries()]);
+
+  const entries = event.formData.entries();
+  for (const entry of entries) {
+    console.log(entry);
   }
-});
+}
 
+form.addEventListener("submit", handleSubmit);
+form.addEventListener("formdata", handleformdata); 
 
 */
+
+/****
+ * 03-transform-formdata (using query string and json):-
+=======================
+import "../assets/css/style.css";
+
+const app = document.getElementById("app");
+app.innerHTML = `
+  <h1>JavaScript DOM</h1>
+    <form  name="order">
+      <label>
+        Your Name
+        <input type="text" name="fullname" />
+      </label>
+      <label>
+        Which pizza would you like?
+        <select name="pizza">
+          <option value="peproni">Peproni</option>
+          <option value="cheesy">Cheesy</option>
+          <option value="meety">Meety</option>
+        </select>
+      </label>
+      <div>
+      What Size ?
+      <label>
+        <input type="radio" name="size" value="small" checked/>
+      </label>
+      <lable>
+        <input type="radio" name="size" value="medim" checked/>
+      </label>
+      <lable>
+        <input type="radio" name="size" value="large" checked/>
+      </label>
+      <label>
+      Quantity?
+        <input type="number" name="quanitity" value="1"/>
+      </label>
+      </div>
+      <input type="submit">
+      </form>
+
+
+`;
+
+const form = document.forms.order;
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const data = [...formData.entries()];
+  
+  // url string
+  // fullname=sarang%20jain&pizza=meety&size=large&quanitity=1
+
+
+  // Another way to create url query string form sending form data
+  const asString2 = new URLSearchParams(formData);
+  console.log(asString2);
+  console.log(asString2.toString());
+
+  // second way to sending data ---> json approach
+
+  const asJson = JSON.stringify(Object.fromEntries(formData));
+  console.log(asJson);
+}
+
+form.addEventListener("submit", handleSubmit);
+
+ */
+
+/***
+
+04-fetch-api-formdata (send form data):-
+=====================================
+
+import "../assets/css/style.css";
+
+const app = document.getElementById("app");
+app.innerHTML = `
+  <h1>JavaScript DOM</h1>
+    <form  name="order">
+      <label>
+        Your Name
+        <input type="text" name="fullname" />
+      </label>
+      <label>
+        Which pizza would you like?
+        <select name="pizza">
+          <option value="peproni">Peproni</option>
+          <option value="cheesy">Cheesy</option>
+          <option value="meety">Meety</option>
+        </select>
+      </label>
+      <div>
+      What Size ?
+      <label>
+        <input type="radio" name="size" value="small" checked/>
+      </label>
+      <lable>
+        <input type="radio" name="size" value="medim" checked/>
+      </label>
+      <lable>
+        <input type="radio" name="size" value="large" checked/>
+      </label>
+      <label>
+      Quantity?
+        <input type="number" name="quanitity" value="1"/>
+      </label>
+      </div>
+      <input type="submit">
+      </form>
+
+
+`;
+
+const form = document.forms.order;
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const data = [...formData.entries()];
+  // url string
+  // fullname=sarang%20jain&pizza=meety&size=large&quanitity=1
+
+  // Another way to create url query string form sending form data
+  const asString2 = new URLSearchParams(formData).toString();
+  // console.log(new URLSearchParams(formData));
+  // console.log(asString2);
+
+  // second way to sending data ---> json approach
+
+  const asJson = JSON.stringify(Object.fromEntries(formData));
+  console.log(asJson);
+
+  fetch("/fakeApi", {
+    method: "post",
+    body: asString2,
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  });
+
+  fetch("/fakeApi2", {
+    method: "post",
+    body: asJson,
+    headers: { "Content-Type": "application/json" },
+  });
+}
+
+form.addEventListener("submit", handleSubmit);
+
+
+ */
